dbafa77e01619e17e57c698e8c716f166009bea4
Joy <joyzeng555@gmail.com>
2019-03-11 21:55:26 -0400

    Reduce image size if needed

--- utils/pdf_convert.py
+++ utils/pdf_convert.py
@@ -17,17 +17,20 @@ def convert_pdf_to_image(pdf_path: str, image_dir: str):
     :param image_dir: The output directory of images.
     :return:
     """
-    dpi = 200   # The image quality in DPI
+    dpi = 100   # The image quality in DPI
     thread_count = 2    # Avoid more than 4
+    quality = 20    # Image resize quality
+    max_pixel = 4096
     image_format = 'jpeg'
     images = convert_from_path(pdf_path, dpi=dpi, fmt=image_format, thread_count=thread_count, output_folder=image_dir)
 
-    # Reduce image size
+    # Reduce image size if one edge's pixel is more than the threshold.
     for image in images:
-        image_path = image.filename
-        max_bytes = 4 * 1024 * 1024
-        while os.stat(image_path).st_size > max_bytes:
-            image.save(image_path, quality=20, optimize=True)
+        if max(image.width, image.height) > max_pixel:
+            image_path = image.filename
+            ratio = max_pixel / max(image.width, image.height)
+            image = image.resize((int(image.width * ratio), int(image.height * ratio)), Image.ANTIALIAS)
+            image.save(image_path, quality=quality, optimize=True)
 
 
 if __name__ == '__main__':
